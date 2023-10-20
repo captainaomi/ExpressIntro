593d4157a4e7e205bc8b58f57a4bbf832562042f
captainaomi <126124875+captainaomi@users.noreply.github.com>
2023-10-21 06:00:36 +1000

    updated from Thursday calss feat: middleware

---
+++ src/controllers/PokemonController.js
@@ -0,0 +1,109 @@
+const express = require('express');
+const {checkForAlex} = require('./PokemonMiddleware');
+const {body, validationResult} = require('express-validator');
+
+// create an instance of the Express router 
+
+const router = express.Router();
+
+// router.use(checkForAlex);
+
+router.get("/", (request, response) => {
+	response.json({
+		message:"Hello world from a router!"
+	});
+});
+
+// GET /pokemon/25 
+router.get("/:numberOfLePokemon", async (request, response) => {
+	let pokemonId = request.params.numberOfLePokemon;
+
+	let result = await fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonId);
+	let data = await result.json();
+
+	response.json({
+		name: data.name
+	});
+});
+
+
+// POST /pokemon/
+// Body: {username:"alex", pokemonId:someNumber}
+
+/*
+router.post(
+	"/",
+	checkForAlex,
+	someOtherMiddleware,
+	WhateverMiddlewareWeCreated,
+	BlahBlahBlah,
+	async (request, response) => {}
+)
+*/
+router.post(
+	"/", 
+	body('username').trim().isLength({min: 4, max: 6}),
+	// checkForAlex, 
+	async (request, response) => {
+
+		const errors = validationResult(request);
+		if (!errors.isEmpty()){
+			return response.status(400).json({
+				message:"You dun goofed",
+				errors: errors.array()
+			});
+		}
+
+
+
+
+	// let pokemonId = request.params.numberOfLePokemon;
+
+	// if (request.body.username != "alex"){
+	// 	return response.json({
+	// 		message:"You are not authorised!"
+	// 	});
+	// }
+	console.log("Post with middleware has run");
+
+	let result = await fetch("https://pokeapi.co/api/v2/pokemon/" + request.body.pokemonId);
+	let data = await result.json();
+
+	response.json({
+		name: data.name,
+		username: request.body.username,
+		pokemonId: request.body.pokemonId  
+
+	});
+});
+
+
+
+
+
+router.get("/bananas", async (request, response) => {
+
+	let result = await fetch("https://pokeapi.co/api/v2/pokemon/25");
+	let data = await result.json();
+
+
+	response.json({
+		message: "bananas!",
+		pokemonName: data.name
+	});
+});
+
+
+// Create out of CRUD 
+router.post("/", (request, response) => {
+	console.log("Post with NO middleware has run!")
+	response.json({
+		message:"POST request received!"
+	})
+});
+
+module.exports = router;
+
+// module.exports = {
+// 	PokemonRouter: router
+// }
--- src/server.js
+++ src/server.js
@@ -1,29 +1,40 @@
-// import the server package
+// import the server package 
 const bananas = require('express');
-// make an instance of the server that we can customise and run
+// make an instance of the server that we can customise and run 
 const app = bananas();
 
 const HOST = process.env.HOST || 'localhost';
 const PORT = process.env.PORT || 3000;
-// Fun fact: ports can only be an integer between 1001 and 65536
+// Ports can only be a integer between 1001 - 65536 
+
+// instance.routes(controllers or middleware)
+// instance.use(middleware)
+// instance.use(router)
+// instance.get(express.json())
+// app.get(bananas.json())
+app.use(bananas.json());
+app.use(bananas.urlencoded({extended: true}));
 
 
-// GET localhost:3000/
+
+// some comment here to trigger nodemon into restarting
+
+// GET localhost:3000/ 
 // app.get(route path, callback function)
 app.get("/", (request, response) => {
 
-    response.send("Hello world, this server is bananas!");
+	response.send("Hello world, this server is bananas!");
 
 });
 
+const someImportedRouterYay = require('./controllers/PokemonController');
+app.use("/pokemon", someImportedRouterYay);
 
-const someImportedRouter = require("./controllers/PokemonController.js");
-app.use("/pokemon", someImportedRouter);
 
 module.exports = {
-    app, 
-    HOST, 
-    PORT
+	app,
+	HOST,
+	PORT
 }
 
-// Configure the server -- happens all above this line ^^^
+// Configure the server -- happens all above this line ^^^^^^
